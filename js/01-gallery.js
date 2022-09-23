82976fa98afbe1c145368ac6bb50a47d87824bb4
Volodymyr-Ogiychuk <volodymyr.ogiychuk@gmail.com>
2022-09-23 22:46:59 +0300

    Add completed HW#7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('div.gallery');
+const markup = [];
+
+galleryItems.forEach(({ preview, original, description }) => {
+    
+    markup.push(`
+
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+      </a>
+    </div>
+  `)    
+})
+
+galleryRef.insertAdjacentHTML('beforeend', markup.join(' '));
+
+galleryRef.addEventListener('click', event => {
+  event.preventDefault();
+  console.log('target', event.target);
+  
+  console.log(event.target.getAttribute("data-source"));
+
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.getAttribute("data-source")}">
+`);
+  instance.show();
+
+  document.addEventListener("keydown", event => {
+
+    if (event.key === "Escape") {
+      instance.close();
+      document.removeEventListener("keydown", event);
+    }
+  
+  })
+
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('ul.gallery');
+const markup = [];
+
+galleryItems.forEach(({ preview, original, description }) => {
+    
+    markup.push(`
+
+    <div class="gallery__item">
+        <a class="gallery__item" href="${original}" data-caption="Описание">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>  
+    </div>
+  `)    
+})
+
+galleryRef.insertAdjacentHTML('beforeend', markup.join(' '));
+
+galleryRef.addEventListener('click', event => {
+  event.preventDefault();
+
+    var lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+
+});
